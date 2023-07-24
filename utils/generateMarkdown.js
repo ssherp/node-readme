9810f6b6632893a1b65d28ad19a6feabdff2c3a5
ssherp <ssherpa89@gmail.com>
2023-07-23 17:34:59 -0700

    update

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,28 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) { }
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseBadge(license) {
+  if (license==="MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  }
+    else if(license==="IBM"){
+    return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"
+  }
+  else if(license==="Apache"){
+    return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+  }
+  else if(license==="boost"){
+    return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
+  }
+  else if(license==="none"){
+  return ""
+  }
+ }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  ${renderLicenseBadge(data.license)}
+  # ${data.title}
   ## description
   ${data.description}
   ## Table of Content 
@@ -34,9 +44,7 @@ function generateMarkdown(data) {
 ${data.usage}
   ## Contact 
   * Github: https://github.com/${data.email}/
-  * Email: ${data.email}
-
-  
+  * Email: ${data.email}  
 
 `;
 }
